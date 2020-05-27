c75eee1c7cc1faffbcd24c192fda8e47942ccc80
xiaodong.li <xiaodong.li@asiainnovations.com>
2020-05-27 22:31:50 +0800

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/json', function(req, res, next) {
+router.get('/json', async function(req, res, next) {
   const url = 'http://' + process.env.ServiceName+ ':'+process.env.ServicePort+'/json';
   let resp = await axios.get(url);
   res.json({ ServiceName: process.env.ServiceName, ServicePort: process.env.ServicePort, resp });
